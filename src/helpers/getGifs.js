2caaaa74ff9307f428336dd39b032acd38deaf39
Osvaldo Gonzalez <ogonzalez@copec.cl>
2021-12-24 15:16:08 -0300

    Cambio de referencias http pot https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=LA6smoNWIANFJLBuIVuyv2izGZMG2G8R`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=LA6smoNWIANFJLBuIVuyv2izGZMG2G8R`;
     const respuesta = await fetch(url);
     const { data } = await respuesta.json();
 
